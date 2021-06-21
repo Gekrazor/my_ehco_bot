00242756594ae67faef507661e44e6377900a32e
Gekrazor <swtorgek@gmail.com>
2021-06-21 15:36:00 +0300

    My first bot

--- # simple_echo_bot.py
+++ # simple_echo_bot.py
@@ -2,11 +2,13 @@
 import telebot
 from telebot.types import Message
 
+# token
 bot = telebot.TeleBot("1741018950:AAHNc5Tz3qbCbPpHMtb3jke1ZYw_NRy3768", parse_mode=None)
 
+# commands
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Greetings! \nUse this messages for more /help /gowild \nOr just lets play in ECHO!")
+	bot.reply_to(message, "Greetings! \nUse this commands for more /help /gowild \nOr just lets play in ECHO!")
 
 @bot.message_handler(commands=['help'])
 def send_wild(message):
@@ -16,7 +18,7 @@ def send_wild(message):
 def send_wild(message):
 	bot.reply_to(message, "MONKE!!! UHUHUHAHAHAH AHUHUHUHAHH")
 
-
+# echo messages
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
